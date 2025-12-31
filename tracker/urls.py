b01cf83370c7665cc9a4a8a8ac8eebd959b0d471
Ramsurya J <139315266+Ramsurya007@users.noreply.github.com>
2025-12-31 21:38:33 +0530

    Add root redirect path to URLs

--- tracker/urls.py
+++ tracker/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('', root_redirect), 
 ]
 urlpatterns += [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
